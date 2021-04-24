61c4d3dd8dee00c64c4b2675b7ecbbdc1ec1d214
Ada <adagraceheart@gmail.com>
2021-04-24 17:31:08 -0500

    update restructure

--- core/src/main/java/lando/systems/ld48/entities/MovableEntity.java
+++ core/src/main/java/lando/systems/ld48/entities/MovableEntity.java
@@ -1,7 +1,5 @@
 package lando.systems.ld48.entities;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
