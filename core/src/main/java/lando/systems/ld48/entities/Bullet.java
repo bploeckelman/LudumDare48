eb7d8abe4de93144baf9041bb9b03f097ef99745
Brian <brossman@kiio.com>
2021-04-25 19:59:54 -0500

    sigh

--- core/src/main/java/lando/systems/ld48/entities/Bullet.java
+++ core/src/main/java/lando/systems/ld48/entities/Bullet.java
@@ -16,14 +16,13 @@ public class Bullet extends GameEntity {
         direction = owner.direction;
         this.bulletSpeed = owner.bulletSpeed * ((direction == Direction.left) ? -1 : 1);
         this.velocity.set(this.bulletSpeed, 0);
-
         initEntity(pos.x, pos.y, keyframe.getRegionWidth() * 0.15f, keyframe.getRegionHeight() * 0.15f);
 
         this.addToScreen(pos.x, pos.y);
     }
 
     public void update(float dt) {
-        velocity.add(bulletSpeed, 0);
+        velocity.set(bulletSpeed, 0);
         super.update(dt);
     }
 }
