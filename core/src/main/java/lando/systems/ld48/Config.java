aa5dd5c2b3b00723c3bf83506a21eabfe8fce63c
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-04-27 15:16:31 -0500

    Set title text to game name

--- core/src/main/java/lando/systems/ld48/Config.java
+++ core/src/main/java/lando/systems/ld48/Config.java
@@ -1,7 +1,7 @@
 package lando.systems.ld48;
 
 public class Config {
-    public static final String title = "Ludum Dare 48";
+    public static final String title = "Deeper State: Elon Flux";
     public static final int windowWidth = 1280;
     public static final int windowHeight = 720;
     public static final int worldWidth = 320;
